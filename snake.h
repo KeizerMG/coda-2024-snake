4ad48d1ec7bfbfb2405d7daeeb60449cd47c6d3f
KeizerMG <mathis.guedy@coda-student.school>
2024-10-18 14:46:26 +0200

    Projet Snake

--- snake.h
+++ snake.h
@@ -1,12 +1,12 @@
-#ifndef _SNAKE_H__
-#define _SNAKE_H__
+#ifndef SNAKE_H
+#define SNAKE_H
 
-#include <time.h>
-#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define WINDOW_WIDTH
-#define WINDOW_HEIGHT 300
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 #define TILE_SIZE 20
 #define GRID_WIDTH (WINDOW_WIDTH / TILE_SIZE)
 #define GRID_HEIGHT (WINDOW_HEIGHT / TILE_SIZE)
@@ -14,21 +14,38 @@
 typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
 
 typedef struct {
-	int x, y;
-} Position; 
+    int x, y;
+} Position;
 
 typedef struct {
-	Position body[GRID_WIDTH * GRID_HEIGHT];
-	int length;
-	Direction direction; 
-} Snake; 
+    Position body[GRID_WIDTH * GRID_HEIGHT];
+    int length;
+    Direction direction;
+} Snake;
 
 typedef struct {
-	Position position;
-} Food; 
+    Position position;
+} Food;
 
 extern Snake snake;
-extern Food food; 
+extern Food food;
+extern SDL_Texture* gameOverTexture;
+extern SDL_Texture* headUpTexture;
+extern SDL_Texture* headRightTexture;
+extern SDL_Texture* headDownTexture;
+extern SDL_Texture* headLeftTexture;
+extern SDL_Texture* bodyVerticalTexture;
+extern SDL_Texture* bodyHorizontalTexture;
+extern SDL_Texture* bodyTopRightTexture;
+extern SDL_Texture* bodyTopLeftTexture;
+extern SDL_Texture* bodyBottomRightTexture;
+extern SDL_Texture* bodyBottomLeftTexture;
+extern SDL_Texture* tailUpTexture;
+extern SDL_Texture* tailRightTexture;
+extern SDL_Texture* tailDownTexture;
+extern SDL_Texture* tailLeftTexture;
+extern SDL_Texture* foodTexture;
+extern SDL_Texture* wallTexture;
 
 int initGame(SDL_Renderer* renderer);
 void initSnake();
@@ -39,4 +56,4 @@ void renderGame(SDL_Renderer* renderer);
 void cleanupGame();
 void changeDirection(Direction newDirection);
 
-
+#endif
